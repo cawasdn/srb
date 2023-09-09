9b82dbd1e0c966832414eadac99e64007c2654fb
Daniel <cawasdn@gmail.com>
2023-09-09 23:00:21 +0800

    Qoute

--- app/page.js
+++ app/page.js
@@ -39,8 +39,10 @@ export default function Home() {
                 </div>
               </div>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Reprehenderit et hic commodi quod illo blanditiis.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+                repellat corrupti magnam rerum numquam explicabo, natus eum
+                corporis accusantium reprehenderit quas quia modi sint officia.
+                Doloremque reprehenderit mollitia praesentium ut?
               </p>
               <div className='flex justify-center gap-6 p-3 pb-6'>
                 <Link href='/projects'>
